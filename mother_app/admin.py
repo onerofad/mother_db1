709e9585bf87936469c75e2c407cdeeb1ab60b23
onerofad <imafidonfrank169@gmail.com>
2026-03-27 11:22:08 +0100

    first commit

--- mother_app/admin.py
+++ mother_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Register, MakeRequest, Support, Chats, Payment
+from .models import Register, MakeRequest, Support, Chats, RegisterWatcher, Cards
 
 # Register your models here.
-admin.site.register({Register, MakeRequest, Support, Chats})
+admin.site.register({Register, MakeRequest, Support, Chats, RegisterWatcher, Cards})
