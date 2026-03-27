a06c0211a0de615ae4f85f2333016b5b4cc28b62
onerofad <imafidonfrank169@gmail.com>
2026-03-27 20:51:28 +0100

    first commit

--- mother_app/models.py
+++ mother_app/models.py
@@ -88,7 +88,7 @@ class Cards(models.Model):
     firstname = models.CharField(max_length=255, default='')
     lastname = models.CharField(max_length=255, default='')
     address1 = models.CharField(max_length=255, default='')
-    address2 = models.CharField(max_length=255, default='')
+    address2 = models.CharField(max_length=255, default='', blank=True)
     country = models.CharField(max_length=255, default='')
     state = models.CharField(max_length=255, default='')
     city = models.CharField(max_length=255, default='')
