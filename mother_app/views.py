c86bb2c81a7293334e7c0380cdd5e75446b6c144
onerofad <imafidonfrank169@gmail.com>
2026-03-30 01:15:22 +0100

    first commit

--- mother_app/views.py
+++ mother_app/views.py
@@ -37,7 +37,7 @@ class CardView(viewsets.ModelViewSet):
     serializer_class = CardSerializer
 
 class PaymentsView(viewsets.ModelViewSet):
-    queryset = Payments.objects.order_by('created_at').all()
+    queryset = Payments.objects.all().order_by('-created_at')
     serializer_class = PaymentsSerializer
 
 class PaymentListView(ListAPIView):
